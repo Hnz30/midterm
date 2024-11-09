ed9becd9bcecae33e4231f18fec065a8c37e826d
Heinz Yumul <johnjayheinzyumul@gmail.com>
2024-11-09 15:28:31 +0800

    created dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php
+require 'functions.php';
+guard();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Welcome to the Dashboard</h1>
+    <p><a href="student/register.php">Manage Students</a></p>
+    <p><a href="subject/add.php">Manage Subjects</a></p>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+</html>
